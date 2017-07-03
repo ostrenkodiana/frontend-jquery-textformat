d9f39af1a120191584f540603ff6150b3e2d1e8d
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-07-03 18:02:29 +0300

    finish all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 'use strict';
+var messageField = $('form .message-field');
+var messageFormatted = $('.message-formatted');
 
-console.log($)
+function getFormattedText(value) {
+    return value.toLowerCase()
+                 .trim()
+                 .replace(/\s+/g,' ');
+
+}
+
+messageField.on('input', function() {
+    var value = messageField.val();
+    var textFormatted = getFormattedText(value);
+    messageFormatted.text(textFormatted);
+});
